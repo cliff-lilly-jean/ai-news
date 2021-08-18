34ba2b2f98e789598bde9e2ce674e0092fe19ff3
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-08-17 22:18:28 -0400

    Integrated the alan library and got ti running

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey = '';
+const alanKey = '1721be4c0a14af6c7313845df5823c852e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
 
  useEffect(() => {
   alanBtn({
-   key: alanKey
+   key: alanKey,
+   onCommand: ({ command }) => {
+    if (command === 'testCommand') {
+     alert('This code was executed');
+    }
+   }
   });
  }, []);
 
