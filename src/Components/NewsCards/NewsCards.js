019574dc67bd6288e034db6eea50ea6173d25730
Cliff Jean <cliffjeanmacpro@cliffs-mac-pro.attlocal.net>
2021-08-19 17:59:09 -0400

    Added material ui to the newscards component

--- src/Components/NewsCards/NewsCards.js
+++ src/Components/NewsCards/NewsCards.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
 
 import NewsCard from '../NewsCard/NewsCard';
 
 const NewsCards = ({ articles }) => {
  return (
-  <div>
+  <Grow in>
+   <Grid container alignItems="stretch" spacing={3}>
+
+   </Grid>
+
+
    {articles.map((article, i) => (
     <NewsCard />
    ))}
-  </div>
+  </Grow>
  );
 };
 
