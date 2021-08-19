303cde12c94c0ae779b9a3c97543cc1d70e3fd34
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-08-19 04:13:33 -0400

    Fixed a bug that was causing the alan app not to retrieve the news articles. Need to fix a new issue that is causing the app to loop through and repeat the command

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './Components/NewsCards/NewsCards';
 
 const alanKey = '1721be4c0a14af6c7313845df5823c852e956eca572e1d8b807a3e2338fdd0dc/stage';
 
+
 const App = () => {
+ const [newsArticles, setNewsArticles] = useState([]);
 
  useEffect(() => {
   alanBtn({
    key: alanKey,
    onCommand: ({ command, articles }) => {
     if (command === 'newHeadlines') {
-     console.log(articles);
+     setNewsArticles(articles);
     }
    }
   });
@@ -18,7 +22,7 @@ const App = () => {
 
  return (
   <div>
-   <h1>Alan Ai news app</h1>
+   <NewsCards articles={newsArticles} />
   </div>
  );
 };
