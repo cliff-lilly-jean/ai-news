61d8975e0053f2b9fc26e0b3d8357536cab3510d
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-08-24 06:19:03 -0400

    Added in the card design and the logo at the top

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './Components/NewsCards/NewsCards';
+import useStyles from './styles';
+import logo from './assets/alan_logo.jpeg';
 
 const alanKey = '1721be4c0a14af6c7313845df5823c852e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 
 const App = () => {
+
+ const classes = useStyles();
  const [newsArticles, setNewsArticles] = useState([]);
 
  useEffect(() => {
@@ -21,8 +25,10 @@ const App = () => {
  }, []);
 
  return (
-  <div>
-   <h1>Alan Ai News</h1>
+  <div className="wrapper">
+   <div className={classes.logoContainer}>
+    <img className={classes.alanLogo} src={logo} alt="Alan Studio Logo" />
+   </div>
    <NewsCards articles={newsArticles} />
   </div>
  );
