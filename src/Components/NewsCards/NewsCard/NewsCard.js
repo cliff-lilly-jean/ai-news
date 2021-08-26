77aeda3db2a0cf1694d61ab01f49eb77c8be4141
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-08-26 06:14:58 -0400

    completed the functionality of the app

--- src/Components/NewsCards/NewsCard/NewsCard.js
+++ src/Components/NewsCards/NewsCard/NewsCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import classNames from 'classnames';
 
@@ -6,8 +6,23 @@ import useStyles from './styles';
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
  const classes = useStyles();
+ const [elRefs, setElRefs] = useState([]);
+ const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+ // On mount create an array of 20 and either populate with j or create new refs
+ useEffect(() => {
+  setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+ }, []);
+
+ // Called whenever i, activeArticle or elRefs changes
+ useEffect(() => {
+  if (i === activeArticle && elRefs[activeArticle]) {
+   scrollToRef(elRefs[activeArticle]);
+  }
+ }, [i, activeArticle, elRefs]);
+
  return (
-  <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+  <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
    <CardActionArea href={url} target="_blank">
     <CardMedia className={classes.media} image={urlToImage || 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.itsmfusa.org%2Fnews%2F&psig=AOvVaw1jGl_4u8RxI1rgehF34QAu&ust=1629524700843000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDP_eDyvvICFQAAAAAdAAAAABAD'} />
     <div className={classes.details}>
