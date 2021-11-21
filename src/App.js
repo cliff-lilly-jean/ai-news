78244acb5699d0b1414843d3bd653e18d06e1671
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-11-21 12:21:10 -0500

    updated the app and fixed a bug that was causing it  not to work

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-import wordsToNumbers from 'words-to-numbers';
-
-import NewsCards from './Components/NewsCards/NewsCards';
-import useStyles from './styles';
 import logo from './assets/alan_logo.jpeg';
-
-const alanKey = '1721be4c0a14af6c7313845df5823c852e956eca572e1d8b807a3e2338fdd0dc/stage';
-
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
 
 const App = () => {
+ const [activeArticle, setActiveArticle] = useState(0);
+ const [newsArticles, setNewsArticles] = useState([]);
+ const [isOpen, setIsOpen] = useState(false);
 
  const classes = useStyles();
- const [newsArticles, setNewsArticles] = useState([]);
-
- // Index of article currently being read
- const [activeArticle, setActiveArticle] = useState(-1);
 
  useEffect(() => {
   alanBtn({
-   key: alanKey,
+   key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
    onCommand: ({ command, articles, number }) => {
     if (command === 'newHeadlines') {
      setNewsArticles(articles);
-     setNewsArticles(-1);
+     setActiveArticle(-1);
+    } else if (command === 'instructions') {
+     setIsOpen(true);
     } else if (command === 'highlight') {
      setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
     } else if (command === 'open') {
-     const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+     const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
      const article = articles[parsedNumber - 1];
 
-     if (parsedNumber > 20) {
-      alanBtn().playText('Please try that again');
+     if (parsedNumber > articles.length) {
+      alanBtn().playText('Please try that again...');
      } else if (article) {
       window.open(article.url, '_blank');
-      alanBtn.playText('Opening...');
+      alanBtn().playText('Opening...');
+     } else {
+      alanBtn().playText('Please try that again...');
      }
     }
-   }
+   },
   });
  }, []);
 
  return (
-  <div className="wrapper">
+  <div>
+   <h1 className="title">Alan AI</h1>
    <div className={classes.logoContainer}>
-    <img className={classes.alanLogo} src={logo} alt="Alan Studio Logo" />
+    {newsArticles.length ? (
+     <div className={classes.infoContainer}>
+      <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+      <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+     </div>
+    ) : null}
+
    </div>
    <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+   <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
   </div>
  );
 };
